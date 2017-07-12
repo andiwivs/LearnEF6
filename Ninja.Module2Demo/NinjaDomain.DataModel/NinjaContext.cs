4b15b62382d6d1945e59a5e60f63fa1ecd018f8d
Andrew Withers <andiwivs@gmail.com>
2017-07-12 23:33:06 +0100

    Update migrations and fix connection string, and add sample business logic to data context definition

--- Ninja.Module2Demo/NinjaDomain.DataModel/NinjaContext.cs
+++ Ninja.Module2Demo/NinjaDomain.DataModel/NinjaContext.cs
@@ -1,5 +1,8 @@
-﻿using NinjaDomain.Classes;
+﻿using System;
+using NinjaDomain.Classes;
 using System.Data.Entity;
+using System.Linq;
+using NinjaDomain.Classes.Interfaces;
 
 namespace NinjaDomain.DataModel
 {
@@ -9,6 +12,46 @@ namespace NinjaDomain.DataModel
         public DbSet<Clan> Clans { get; set; }
         public DbSet<NinjaEquipment> Equipment { get; set; }
 
-        //public NinjaContext() : base("NinjaDomainConnectionString") { }
+        public NinjaContext() : base("NinjaDomainConnectionString") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // do not track IsDirty property in data model
+            modelBuilder
+                .Types()
+                .Configure(t => t.Ignore("IsDirty"));
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            // performing sweeping update to auto-maintain created/modified dates for entities implementing our IModificationHistory interface...
+            ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is IModificationHistory)
+                .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified)
+                .Select(e => e.Entity as IModificationHistory)
+                .ToList()
+                .ForEach(h =>
+                {
+                    h.DateModified = DateTime.Now;
+
+                    if (h.DateCreated == DateTime.MinValue)
+                        h.DateCreated = DateTime.Now;
+                });
+
+            int result = base.SaveChanges();
+
+            // for local model (and not persisted in data model as configured in OnModelCreating above) reset IsDirty flags
+            ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is IModificationHistory)
+                .Select(e => e.Entity as IModificationHistory)
+                .ToList()
+                .ForEach(h => h.IsDirty = false);
+
+            return result;
+        }
     }
 }
--- Ninja.Module2Demo/NinjaDomain.DataModel/README.txt
+++ Ninja.Module2Demo/NinjaDomain.DataModel/README.txt
@@ -1,3 +1,16 @@
 ﻿From NuGet Package Manager Console, with DataModel project selected:
 
+> enable-migrations          # creates Migrations folder with basic configuration class
+
+> add-migration Initial      # adds first migration script to Migrations folder, will be named "Initial"
+                             # NOTE: add-migration only works when data model project is active startup project
+
+> update-database -script    # generates a SQL script to create schema. Based on app.config settings, by default script generator assumes SQL server with local db
+
+> update-database -verbose   # executes schema migrations directly based on config
+
+
+Having later made changes to model classes:
+
+> add-migration AddSomethingNew
 > update-database -verbose
